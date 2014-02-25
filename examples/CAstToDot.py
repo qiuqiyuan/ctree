9fc6c446df18922daf7ac50fedcae04db96b3136
Leonard Truong <leonardtruong@gmail.com>
2014-02-24 22:32:41 -0800

    Move visualize to separate test

--- examples/CAstToDot.py
+++ examples/CAstToDot.py
@@ -14,7 +14,7 @@ def main():
   stmt0 = Assign(SymbolRef('foo'), Constant(123.4))
   stmt1 = FunctionDecl(Float(), SymbolRef("bar"), [Int(), Long()], [String("baz")])
   tree = File([stmt0, stmt1])
-  print (tree.visualize())
+  print (tree.to_dot())
 
 if __name__ == '__main__':
   main()
