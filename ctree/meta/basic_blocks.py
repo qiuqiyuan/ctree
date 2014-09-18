0be126b78e374a14bef8fd7810cc85f285b0b9ca
Leonard Truong <leonardtruong@gmail.com>
2014-09-18 12:44:59 -0700

    Refactor basic blocks to use python ast nodes

---
+++ ctree/meta/basic_blocks.py
@@ -0,0 +1,178 @@
+import ast
+from functools import reduce
+from .nodes import Assign, Symbol, Return, Param, Constant, FunctionCall,\
+    Attribute
+import sys
+from ctree.jit import LazySpecializedFunction
+
+
+def eval_in_env(expr, env):
+    if isinstance(expr, Symbol):
+        return env[expr.name]
+    elif isinstance(expr, Attribute):
+        return getattr(env[expr.name], expr.attr)
+
+
+def str_dump(item):
+    if isinstance(item, ast.Assign):
+        return "{} = {}".format(str_dump(item.targets[0]),
+                                str_dump(item.value))
+    elif isinstance(item, ast.Return):
+        return "return {}".format(str_dump(item.value))
+    elif isinstance(item, ast.Attribute):
+        return "{}.{}".format(str_dump(item.value), item.attr)
+    elif isinstance(item, ast.Call):
+        return "{}({})".format(str_dump(item.func),
+                               ", ".join(map(str_dump, item.args)))
+    elif isinstance(item, ast.Num):
+        return str(item.n)
+    elif isinstance(item, ast.Name):
+        return item.id
+    raise Exception("Unsupport type for dumping {}: {}".format(type(item), item))
+
+
+class BasicBlock(object):
+    def __init__(self, name, params, body, composable_blocks=None):
+        self.name = name
+        self.params = params
+        self.body = body
+        if composable_blocks is None:
+            self.composable_blocks = []
+        else:
+            self.composable_blocks = composable_blocks
+
+    def find_composable_blocks(self, env):
+        statements = []
+        composable_statements = []
+        composable_blocks = []
+        for statement in self.body:
+            print(statement)
+            if isinstance(statement, Assign) and \
+               isinstance(statement.value, FunctionCall) and \
+               isinstance(eval_in_env(env, statement.value.func),
+                          LazySpecializedFunction):
+                composable_statements.append(statement)
+            else:
+                if len(composable_statements) > 1:
+                    composable_block = ComposableBlock(composable_statements)
+                    statements.append(composable_block)
+                    composable_blocks.append(composable_block)
+                elif len(composable_statements) == 1:
+                    statements.append(composable_statements[0])
+                composable_statements = []
+
+        self.statements = statements
+        self.composable_blocks = composable_blocks
+
+    def __len__(self):
+        return len(self.body)
+
+    def __getitem__(self, item):
+        return self.body[item]
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __repr__(self):
+        return """
+BasicBlock
+  Name: {name}
+  Params: {params}
+  Body:
+    {body}
+""".format(name=self.name,
+           params=", ".join(self.params),
+           body="\n    ".join(map(str_dump, self.body)))
+
+
+class ComposableBlock(object):
+    """docstring for ComposableBlock"""
+    def __init__(self, statements):
+        super(ComposableBlock, self).__init__()
+        self.statements = statements
+
+    def __str__(self):
+        return """
+ComposableBlock
+  {}
+""".format("\n  ".join(map(str, self.statements)))
+
+
+class BlockDecomposer(object):
+    def __init__(self):
+        self.__curr_tmp = -1
+
+    def gen_tmp(self):
+        self.__curr_tmp += 1
+        return "_t{}".format(self.__curr_tmp)
+
+    def visit(self, expr, curr_target=None):
+        if isinstance(expr, ast.Return):
+            tmp = self.gen_tmp()
+            body = self.visit(expr.value, ast.Name(tmp, ast.Store()))
+            body.append(ast.Return(ast.Name(tmp, ast.Load())))
+        elif isinstance(expr, ast.Name):
+            return expr
+        elif isinstance(expr, ast.BinOp):
+            body = []
+            operands = []
+            for operand in [expr.left, expr.right]:
+                if isinstance(operand, (ast.Name, ast.Num)):
+                    operands.append(operand)
+                else:
+                    tmp = self.gen_tmp()
+                    body.extend(self.visit(operand,
+                                           ast.Name(tmp, ast.Store())))
+                    operands.append(ast.Name(tmp, ast.Load()))
+            if isinstance(expr.op, ast.Add):
+                op = ast.Attribute(operands[0], '__add__', ast.Load())
+            elif isinstance(expr.op, ast.Mult):
+                op = ast.Attribute(operands[0], '__mul__', ast.Load())
+            elif isinstance(expr.op, ast.Sub):
+                op = ast.Attribute(operands[0], '__sub__', ast.Load())
+            elif isinstance(expr.op, ast.Div):
+                op = ast.Attribute(operands[0], '__div__', ast.Load())
+            else:
+                raise Exception("Unsupported BinOp {}".format(expr.op))
+            body.append(ast.Assign([curr_target],
+                                   ast.Call(op, operands, [], None, None)))
+        elif isinstance(expr, ast.Assign):
+            target = expr.targets[0]
+            body = self.visit(expr.value, target)
+        elif isinstance(expr, ast.Call):
+            body = []
+            args = []
+            for arg in expr.args:
+                val = self.visit(arg)
+                if isinstance(val, list):
+                    tmp = self.gen_tmp()
+                    val = self.visit(arg, ast.Name(tmp, ast.Store))
+                    body.extend(val)
+                    args.append(ast.Name(tmp, ast.Load()))
+                elif isinstance(val, (ast.Name, ast.Num)):
+                    args.append(val)
+                else:
+                    raise Exception("Call argument returned\
+                                     unsupported type {}".format(type(val)))
+            if curr_target is not None:
+                body.append(ast.Assign([curr_target],
+                                       ast.Call(self.visit(expr.func), args, [], None, None)))
+            else:
+                body.append(ast.Call(self.visit(expr.func), args))
+        else:
+            raise Exception("Unsupported expression {}".format(expr))
+        return body
+
+
+def get_basic_block(module):
+    func = module.body[0]
+    decomposer = BlockDecomposer()
+    if sys.version_info > (3, 0):
+        params = [arg.arg for arg in func.args.args]
+    else:
+        params = [arg.id for arg in func.args.args]
+    body = map(decomposer.visit, func.body)
+    body = reduce(lambda x, y: x + y, body, [])
+    return BasicBlock(func.name, params, body)
+
+
---
+++ tests/meta/test_basic_blocks.py
@@ -0,0 +1,217 @@
+import unittest
+from ctree.frontend import get_ast
+
+from ctree.meta.blocks.basic_blocks import get_basic_block
+from ctree.meta.blocks.nodes import Symbol, Constant, Return
+
+import ast
+
+from ctree.jit import LazySpecializedFunction
+
+
+class TestBasicBlockBuilder(unittest.TestCase):
+    def _check_args(self, actual, expected):
+        for act, exp in zip(actual, expected):
+            if isinstance(act, Symbol):
+                self.assertEqual(act.id, exp)
+            elif isinstance(act, Constant) and act.value is not exp:
+                self.assertEqual(act.value, exp)
+
+    def test_simple_return(self):
+        def func(a, b):
+            return a + b
+
+        tree = get_ast(func)
+        basic_block = get_basic_block(tree)
+        self.assertEqual(len(basic_block), 2)
+        self.assertEqual(basic_block[0].targets[0].id, '_t0')
+        self.assertEqual(
+            basic_block[0].value.func.value.id, 'a')
+        self.assertEqual(
+            basic_block[0].value.func.attr, '__add__')
+        self._check_args(basic_block[0].value.args, ['a', 'b'])
+        self.assertIsInstance(basic_block[1], ast.Return)
+        self.assertEqual(basic_block[1].value.id, '_t0')
+
+    def test_simple_body(self):
+        def func(a, b):
+            c = a * b
+            return c * 3
+
+        tree = get_ast(func)
+        basic_block = get_basic_block(tree)
+        self.assertEqual(len(basic_block), 3)
+        self.assertEqual(basic_block[0].targets[0].id, 'c')
+        self.assertEqual(
+            basic_block[0].value.func.value.id, 'a')
+        self.assertEqual(
+            basic_block[0].value.func.attr, '__mul__')
+        self._check_args(basic_block[0].value.args, ['a', 'b'])
+        self.assertEqual(basic_block[1].targets[0].id, '_t0')
+        self.assertEqual(
+            basic_block[1].value.func.value.id, 'c')
+        self.assertEqual(
+            basic_block[1].value.func.attr, '__mul__')
+        self._check_args(basic_block[1].value.args, ['c', 3])
+        self.assertIsInstance(basic_block[2], ast.Return)
+        self.assertEqual(basic_block[2].value.id, '_t0')
+
+    def test_unpack_expression(self):
+        def func(a, b, c):
+            return a * b + c
+
+        tree = get_ast(func)
+        basic_block = get_basic_block(tree)
+        print(basic_block)
+        self.assertEqual(len(basic_block), 3)
+        self.assertEqual(basic_block[0].targets[0].id, '_t1')
+        self.assertEqual(
+            basic_block[0].value.func.value.id, 'a')
+        self.assertEqual(
+            basic_block[0].value.func.attr, '__mul__')
+        self._check_args(basic_block[0].value.args, ['a', 'b'])
+        self.assertEqual(basic_block[1].targets[0].id, '_t0')
+        self.assertEqual(
+            basic_block[1].value.func.value.id, '_t1')
+        self.assertEqual(
+            basic_block[1].value.func.attr, '__add__')
+        self._check_args(basic_block[1].value.args, ['_t1', 'c'])
+        self.assertIsInstance(basic_block[2], ast.Return)
+        self.assertEqual(basic_block[2].value.id, '_t0')
+
+    def test_unpack_precedence(self):
+        def func(a, b, c):
+            return a + b * c
+
+        tree = get_ast(func)
+        basic_block = get_basic_block(tree)
+        print(basic_block)
+        self.assertEqual(len(basic_block), 3)
+        self.assertEqual(basic_block[0].targets[0].id, '_t1')
+        self.assertEqual(
+            basic_block[0].value.func.value.id, 'b')
+        self.assertEqual(
+            basic_block[0].value.func.attr, '__mul__')
+        self._check_args(basic_block[0].value.args, ['b', 'c'])
+        self.assertEqual(basic_block[1].targets[0].id, '_t0')
+        self.assertEqual(
+            basic_block[1].value.func.value.id, 'a')
+        self.assertEqual(
+            basic_block[1].value.func.attr, '__add__')
+        self._check_args(basic_block[1].value.args, ['a', '_t1'])
+        self.assertIsInstance(basic_block[2], ast.Return)
+        self.assertEqual(basic_block[2].value.id, '_t0')
+
+    def test_simple_function_call(self):
+        def z(a):
+            return a
+
+        def func(a):
+            return z(a)
+
+        tree = get_ast(func)
+        basic_block = get_basic_block(tree)
+        print(basic_block)
+        self.assertEqual(len(basic_block), 2)
+        self.assertEqual(basic_block[0].targets[0].id, '_t0')
+        self.assertIsInstance(basic_block[0].value, ast.Call)
+        self.assertEqual(basic_block[0].value.func.id, 'z')
+        self.assertEqual(basic_block[0].value.args[0].id, 'a')
+        self.assertIsInstance(basic_block[1], ast.Return)
+        self.assertEqual(basic_block[1].value.id, '_t0')
+
+    def test_function_call_expr_arg(self):
+        def z(a):
+            return a
+
+        def func(a, b):
+            return z(a + b)
+
+        tree = get_ast(func)
+        basic_block = get_basic_block(tree)
+        print(basic_block)
+        self.assertEqual(len(basic_block), 3)
+        self.assertEqual(basic_block[0].targets[0].id, '_t1')
+        self.assertIsInstance(basic_block[0].value, ast.Call)
+        self.assertEqual(basic_block[0].value.func.value.id, 'a')
+        self.assertEqual(basic_block[0].value.func.attr, '__add__')
+        self._check_args(basic_block[0].value.args, ['a', 'b'])
+        self.assertEqual(basic_block[0].targets[0].id, '_t1')
+        self.assertIsInstance(basic_block[0].value, ast.Call)
+        self.assertEqual(basic_block[1].value.func.id, 'z')
+        self._check_args(basic_block[1].value.args, ['_t1'])
+        self.assertIsInstance(basic_block[2], ast.Return)
+        self.assertEqual(basic_block[2].value.id, '_t0')
+
+
+class TestBasicBlockPrint(unittest.TestCase):
+    def _check(self, func, expected):
+        block = get_basic_block(get_ast(func))
+        self.assertEqual(repr(block), expected)
+
+    def test_simple(self):
+        def func(a, b):
+            return a + b
+
+        self._check(func, """
+BasicBlock
+  Name: func
+  Params: a, b
+  Body:
+    _t0 = a.__add__(a, b)
+    return _t0
+""")
+
+    def test_multi_line(self):
+        def z(a, b):
+            return a + b
+
+        def func(a, b):
+            c = a * b
+            d = z(c)
+            return d * z(a + b, a - b)
+
+        self._check(func, """
+BasicBlock
+  Name: func
+  Params: a, b
+  Body:
+    c = a.__mul__(a, b)
+    d = z(c)
+    _t2 = a.__add__(a, b)
+    _t3 = a.__sub__(a, b)
+    _t1 = z(_t2, _t3)
+    _t0 = d.__mul__(d, _t1)
+    return _t0
+""")
+
+
+class TestLSF(LazySpecializedFunction):
+    pass
+
+
+class TestComposableBlocks(unittest.TestCase):
+    def test_no_composable(self):
+        def func(a, b):
+            return a + b
+
+        tree = get_ast(func)
+        basic_block = get_basic_block(tree)
+        basic_block.find_composable_blocks(dict(globals(), **locals()))
+        self.assertEqual(len(basic_block.composable_blocks), 0)
+
+    def test_one_composable(self):
+        lsf = TestLSF(None)
+        a = 3
+        b = 1
+
+        def func(a, b):
+            a = lsf(a)
+            b = lsf(b)
+            return a + b
+
+        tree = get_ast(func)
+        basic_block = get_basic_block(tree)
+        basic_block.find_composable_blocks(dict(globals(), **locals()))
+        self.assertEqual(len(basic_block.composable_blocks), 1)
+        print(basic_block)
