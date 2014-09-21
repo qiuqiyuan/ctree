29a5e3557af524caffaae0fdc94ea1c34e7f6d53
Leonard Truong <leonardtruong@gmail.com>
2014-09-21 11:44:56 -0700

    Formatting.

--- ctree/meta/core.py
+++ ctree/meta/core.py
@@ -1,8 +1,9 @@
 from ctree.frontend import get_ast
-from .basic_blocks import get_basic_block, find_composable_blocks, process_composable_blocks, get_callable
+from .basic_blocks import get_basic_block, find_composable_blocks, \
+    process_composable_blocks
 import inspect
 import sys
-from copy import deepcopy
+# from copy import deepcopy
 import ast
 
 
@@ -17,10 +18,11 @@ def meta(func):
         for frame in inspect.stack()[1:]:
             symbol_table.update(frame[0].f_locals)
             for index, arg in enumerate(args):
+                arg = original_ast.body[0].args.args[index]
                 if sys.version_info >= (3, 0):
-                    symbol_table[original_ast.body[0].args.args[index].arg] = arg
+                    symbol_table[arg.arg] = arg
                 else:
-                    symbol_table[original_ast.body[0].args.args[index].id] = arg
+                    symbol_table[arg.id] = arg
         basic_block = find_composable_blocks(orig_basic_block, symbol_table)
         basic_block = process_composable_blocks(basic_block, symbol_table)
         callable = get_callable(basic_block, symbol_table, args)
@@ -38,7 +40,8 @@ def my_exec(func, env):
 
 def get_callable(basic_block, env, args):
     tree = ast.Module(
-        [ast.FunctionDef(basic_block.name, basic_block.params, list(basic_block.body), [])]
+        [ast.FunctionDef(basic_block.name, basic_block.params,
+                         list(basic_block.body), [])]
     )
     ast.fix_missing_locations(tree)
     my_exec(compile(tree, filename="tmp", mode="exec"), env)
